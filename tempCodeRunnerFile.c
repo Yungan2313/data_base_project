b7f8d3f09ba9d44475f809cfaa2d639034b32a8a
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2023-05-02 11:40:36 +0800

    測試.txt檔讀取成功
    若執行時沒宥用當時建構的環境
    執行檔會出現在一個新的output資料夾內，更改執行的選項為run code就可以新增在原檔案夾位置

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+    FILE *p;
+    p = fopen("test.txt", "r");
+    if (p == NULL) {
+      printf("xxx!\n");
+      return 0;
+    }
+}
---
+++ txtinput.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+    FILE *p;
+    p = fopen("test.txt", "r");
+    if (p == NULL) {
+      printf("xxx!\n");
+      return 0;
+    }
+    else{
+        printf("ok");
+    }
+}
