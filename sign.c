acf53b4a7d90e9eae30a39033fea02afea878f08
uaena030 <112924804+uaena030@users.noreply.github.com>
2023-05-16 21:52:05 +0800

    5/16-2
    meow

--- sign.c
+++ sign.c
@@ -4,7 +4,9 @@
 //帳密總字數限20、至少2個大寫2個小寫、不可空白
 
 void account(){
-
+    char* account_name;
+    account_name = (char*)malloc(sizeof(char) * 21);
+    fgets(account_name, 21, stdin);
 }
 void password(){
 
