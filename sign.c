03b15bf258da5a62e0c5986e331ee812220652e3
uaena030 <112924804+uaena030@users.noreply.github.com>
2023-06-02 23:39:58 +0800

    6/2
    sign version1.0

--- sign.c
+++ sign.c
@@ -1,111 +1,102 @@
 #include "basic.h"
 #include "txtinput.h"
 
-//帳號：總字數限20、至少2個大寫2個小寫、要有數字、不可空白 //不能跟其他帳號重複
-void account(struct acpd_list **list, char *account_name){
+char *account();
+static char *password(char *account_name);
+
+    // 帳號：總字數限20、不可空白、不能含特殊字元(除了英文跟數字以外的) //不能跟其他帳號重複
+char *account(){
+    int access = 0, choice;
+    char* account_name = (char*)malloc(sizeof(char) * 21);
+    struct acpd_list *new_node = acpd_read("acpd.txt");
+    struct acpd_list *ptr;
+    while(access != 1){
+        origin:
+        printf("Do you want to create a new account?\n[1]Yes\t[2]No\n");
+        scanf("%d", &choice);
+        if(choice == 1){
+            printf("Input your account\n");
+            fgets(account_name, 21, stdin);
+            for (int i = 0; i < 21; i++){
+                if (~(*(account_name + i) >= 'Z' && *(account_name + i) <= 'A') && 
+                    ~(*(account_name + i) >= 'z' && *(account_name + i) <= 'a') &&
+                    ~(*(account_name + i) >= '9' && *(account_name + i) <= '0')){
+                    printf("Invalid account.\n");
+                    printf("Please try a new name\n");
+                    goto origin;
+                }
+                else if(*(account_name + i) == ' '){
+                    printf("Space character is forbidden.\n");
+                    printf("Please try a new password");
+                    goto origin;
+                }
+            }
+            for(ptr = new_node; ptr != NULL; ptr = ptr->next){
+                if(strcmp(ptr->account, account_name) == 0){
+                    printf("Account already exists\n");
+                    goto origin;
+                }
+            }
+            char *password_total, register_new;
+            strcpy(password_total, password(account_name));
+            register_new = acpd_write(account_name, password_total);
+            return "acpd.txt";
+        }
+        else{
+            return NULL;
+        }
+    }
+}
+
+// 密碼：至少2個大寫2個小寫、要有數字、字數限20、不可空白、不能跟帳號重複(strcat)或與帳號相似(strstr、非嚴格比對)
+static char* password(char* account_name){
     int access = 0;
-    acpd_read('acpd.txt');
-    account_name = (char*)malloc(sizeof(char) * 21);
-    struct acpd_list *new_node = malloc(sizeof(struct acpd_list));
-    new_node = list;
-    account_name = new_node->account;
+    char *corres_password = (char *)malloc(sizeof(char) * 21);
     origin:while(access != 1){
-        if(account_name == NULL){
-            printf("Where is your account?\n");
-            printf("Please retype it again\n");
+        printf("Input your password\n");
+        fgets(corres_password, 21, stdin);
+        if(strcmp(corres_password, account_name) == 0){
+            printf("Your password is the same as your account");
+            printf("Please try a new password");
             goto origin;
         }
         int big_word = 0, small_word = 0, number = 0;
         for(int i = 0; i < 21; i++){
-            if(*(account_name + i) <= 'Z' && *(account_name + i) >= 'A'){
+            if (*(corres_password + i) <= 'Z' && *(corres_password + i) >= 'A'){
                 big_word++;
             }
-            else if (*(account_name + i) <= 'z' && *(account_name + i) >= 'a'){
+            else if (*(corres_password + i) <= 'z' && *(corres_password + i) >= 'a'){
                 small_word++;
             }
-            else if (*(account_name + i) <= '9' && *(account_name + i) >= '0'){
+            else if (*(corres_password + i) <= '9' && *(corres_password + i) >= '0'){
                 number++;
+            }
+            else if (*(corres_password + i) == ' '){
+                printf("Space character is forbidden.\n");
+                printf("Please try a new password");
+                goto origin;
             }
         }
         if(big_word < 2 || small_word < 2 || number == 0){
             printf("Your account name is in the wrong format.\n");
             printf("Please retype it again\n");
-        }
-        else{
-            new_node = new_node->next;
-            access = 1;
-        }
-    }
-}
-#ifdef DEBUG
-int similiar(char *corres_password, char *account_name){
-    int k = 0, g = 0;
-    for (int j = 0; *(corres_password + j) != '\0' && *(account_name + j) != '\0'; j++){
-        if (*(corres_password + j) < 'Z' && *(corres_password + j) > 'A' &&\
-            *(account_name + j) < 'Z' && *(account_name + j) > 'A' &&\
-            *(corres_password + j) < 'z' && *(corres_password + j) > 'a' &&\
-            *(account_name + j) < 'z' && *(account_name + j) > 'a'){
-                *(corres_password + j) = tolower(*(corres_password + j));
-                *(account_name + j) = tolower(*(account_name + j));
-            }
-    }
-    for (int j = 0; *(account_name + j) != '\0'; j++){
-        k++;
-    }                
-    char *tmpstring;
-    tmpstring = (char *)malloc(sizeof(char) * (k + 1));
-    for (int j = 0; *(account_name + j) != '\0'; j++){
-        *(tmpstring + j) = *(account_name + j);
-    }
-    *(tmpstring + k) = '\0';
-    if (*(corres_password) != *(tmpstring)){
-            printf("No longest common prefix\n");
-            return 0;
-    }
-    for (int j = 0; *(corres_password + j) != '\0'; j++){
-        
-    }
-    for (int j = 0; j < k; j++){
-        printf("%c", *(tmpstring + j));
-    }
-    return 0;
-}
-#endif
-
-//密碼：不能含特殊字元(除了英文跟數字以外的)、字數限20、不可空白、不能跟帳號重複(strcat)或與帳號相似(strstr、非嚴格比對)
-void password(struct acpd_list **list, char *corres_password, char *account_name){
-    int access = 0;
-    struct acpd_list *new_node = malloc(sizeof(struct acpd_list));
-    new_node = list;
-    corres_password = (char *)malloc(sizeof(char) * 21);
-    account_name = new_node->account;
-    corres_password = new_node->password;
-    origin:while(access != 1){
-        if(corres_password == NULL){
-            printf("Where is your account?\n");
-            printf("Please try a new password");
             goto origin;
         }
-        else if(strcat(corres_password, account_name) == 1){
-            printf("Your password is the same as your account");
-            printf("Please try a new password");
+        if (*(corres_password) = '\n'){
+            printf("Where is your password?\n");
+            printf("Please retype it again\n");
             goto origin;
         }
-        for(int i = 0; i < 21; i++){
-            if (~(*(account_name + i) >= 'Z' && *(account_name + i) <= 'A') &&\ 
-            ~(*(account_name + i) >= 'z' && *(account_name + i) <= 'a') &&\
-            ~(*(account_name + i) >= '9' && *(account_name + i) <= '0')){
-                printf("Invalid password.\n");
-                printf("Please try a new password\n");
-                goto origin;
-            }
-        }
-        char *similarity = (char *)malloc(sizeof(char) * 21);
-        if (similarity = strstr(account_name, corres_password) != NULL && strlen(similarity) >= 5){
+        if (strstr(account_name, corres_password) != NULL && strlen(strstr(account_name, corres_password)) >= 5){
             printf("Your password is too easy\n");
             goto origin;
         }
-        new_node = new_node->next;
+        return corres_password;
         access = 1;
     }
+}
+
+int main(){
+    account();
+    return 0;
 }
