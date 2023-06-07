7f4a65945efdad4a4c5df72c4a8d1fbe4a57d568
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2023-06-08 02:32:47 +0800

    第四次進度part2
    本次進度:
    action.c
    1.add_list 完成
    配合comment的新增檔案
    空格替換成_以防txt內的切割規則出錯
    會檢查是否重複將同樣的food_store再次輸入
    2.delete_list 完成
    將配合UI的介面來刪除特定資料
    同樣會配合comment的刪除
    3.search_list 完成
    透過不同的要求，去尋找相對應的所有資料(其中要記得新增一個新的linked list去儲存蒐尋回傳的指標，而退出時記得要用原本的linked list和清空新搜尋linked list)
    4.log_out 完成
    將最後更動完的database資料存回txt檔儲存，同樣有做結尾清空
    linked_list
    1.刪除不必要的註解
    2.delete_linked_list 完成
    將特定位置的node刪除
    3.三種不同的linked list清除
    下次進度:
    預計會是main.c統整

--- basic.h
+++ basic.h
@@ -3,3 +3,4 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <ctype.h>
--- linked_list.c
+++ linked_list.c
@@ -69,67 +69,68 @@ void insert_front_comment(struct comment_list **list,char account[],float score,
         return;
     }
 }
-/*struct linked_list *search(struct linked_list **list,char food_name[],char store_name[],int price,float score){
-    struct linked_list *node = *list;
 
-
-    // switch(choose){
-    //     case 0://food_name
-    //         char *food_name;
-    //         printf("input search:");
-    //         fgets(food_name,sizeof(char),stdin);
-    //         int len = strlen(food_name);
-    //         *(food_name+len-1) = '\0';
-    //         while(node->next != NULL){
-    //             if(strcmp(food_name,node->food_name)){
-    //                 return node;
-    //             }
-    //             node = node->next;
-    //         }
-    //         break;
-    //     case 1://store_name
-    //         char *store_name;
-    //         printf("input search:");
-    //         fgets(store_name,sizeof(char),stdin);
-    //         int len = strlen(store_name);
-    //         *(store_name+len-1) = '\0';
-    //         while(node->next != NULL){
-    //             if(strcmp(store_name,node->store_name)){
-    //                 return node;
-    //             }
-    //             node = node->next;
-    //         }
-    //         break;
-    //     case 2://price
-    //         int price;
-    //         printf("input search:");
-    //         scanf("%d",&price);
-    //         while(node->next != NULL){
-    //             if(node->price == price){
-    //                 return node;
-    //             }
-    //             node = node->next;
-    //         }
-    //         break;
-    //     case 3://score
-    //         float score;
-    //         printf("input search:");
-    //         scanf("%d",&score);
-    //         while(node->next != NULL){
-    //             if(node->score == score){
-    //                 return node;
-    //             }
-    //             node = node->next;
-    //         }
-    //         break;
-    //     default:
-    //         printf("search failed!\n");
-    //         return NULL;
-    //}
-}*/
-void insert_position(){
-
+struct linked_list *delete_linked_list(struct linked_list *list,int position){
+    struct linked_list *new_node;
+    if(position == 0){
+        return list->next;
+    }
+    for(int i = 0;i<position;i++){
+        new_node = new_node->next;
+    }
+    new_node->next = new_node->next->next;
+    return list;
 }
-void delete_position(){
 
+//將空間釋放
+void free_linked_list(struct linked_list *list){
+    struct linked_list *prev = list;
+    if(list == NULL){
+        return;
+    }
+    if(list->next == NULL){
+        free(list);
+        return;
+    }
+    list = list->next;
+    while(list!=NULL){
+        free(prev);
+        prev = list;
+        list = list->next;
+    }
+    free(prev);
+}
+void free_acpd_list(struct acpd_list *list){
+    struct acpd_list *prev = list;
+    if(list == NULL){
+        return;
+    }
+    if(list->next == NULL){
+        free(list);
+        return;
+    }
+    list = list->next;
+    while(list!=NULL){
+        free(prev);
+        prev = list;
+        list = list->next;
+    }
+    free(prev);
+}
+void free_comment_list(struct comment_list *list){
+    struct comment_list *prev = list;
+    if(list == NULL){
+        return;
+    }
+    if(list->next == NULL){
+        free(list);
+        return;
+    }
+    list = list->next;
+    while(list!=NULL){
+        free(prev);
+        prev = list;
+        list = list->next;
+    }
+    free(prev);
 }
