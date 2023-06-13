1825eb860f390911906563e9394a3c36dc8f1266
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2023-06-13 21:27:12 +0800

    UI 優化

--- UI.c
+++ UI.c
@@ -5,30 +5,39 @@
 //#define DEBUG_UI
 int database_UI(struct linked_list *list,int page){
     struct linked_list *node = list;
-
+    int food_name_length,store_name_length,fpadd,spadd;
     for(int i = 0;i<page*10;node = node->next){
+        printf("%d:%s\n",i,node->food_name);
         if(node == NULL){
            return -1;
         }
         i++;
     }
-    node = list;
-    printf("|  ID  |      Name      |     Store     |  Price  |  Score  |\n");
-    for(int l = 0;l <= 63; l++){
+    printf("|  ID  |        Name        |       Store       |  Price  |  Score  |\n");
+    for(int l = 0;l <= 70; l++){
             printf("-");
     }
     printf("\n");
     
     for(int i = 0;i < 10;node = node->next){
+        food_name_length = strlen(node->food_name);
+        store_name_length = strlen(node->store_name);
+        fpadd = (20 - food_name_length) / 2;
+        food_name_length%2==1?food_name_length++:fpadd;
+        spadd = (20 - store_name_length) / 2;
+        store_name_length%2==0?store_name_length--:spadd;
+        // printf("%d:%d %d\n",i,fpadd,spadd);
         if(node != NULL){
-            printf("|%4d  |%*s      |%*s     |%*d  |%*.1f  |\n",i, 10, node->food_name, 10, node->store_name, 7, node->price, 7, node->score);
+            // printf("|%4d  |%*s%-*s|%*s%-*s|%*d  |%*.1f  |\n",i,10 + fpadd, node->food_name, 10 - fpadd, "",10 + spadd, node->store_name, 10 - spadd, "",7,7,node->price,7,node->score);
+            // printf("|%4d  |%*s|%*s|%*d  |%*.1f  |\n",i, 20, node->food_name, 20, node->store_name, 7, node->price, 7, node->score);
+            printf("|%4d  |%*s%*s|%*s%*s|%*d  |%*.1f  |\n",i, fpadd+food_name_length, node->food_name,fpadd,"", spadd+store_name_length, node->store_name,spadd,"", 7, node->price, 7, node->score);
         }else{
             break;
         }
         i++;
     }
 
-    for(int l = 0;l <= 63; l++){
+    for(int l = 0;l <= 70; l++){
             printf("-");
     }
     printf("\n");
